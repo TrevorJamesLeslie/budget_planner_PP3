9cf3d17fae913a97ffb2d02ee0dda10b923a438e
Monika Mak <monika1986mak@gmail.com>
2024-07-02 13:44:14 +0000

    activate  API and  connection with goolge sheet

--- run.py
+++ run.py
@@ -8,3 +8,19 @@ SCOPE = [
     
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+##const for security, file not to be tracked 
+CREDS = Credentials.from_service_account_file('creds.json')
+#const for credentials scoped
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+#const to allow auth of gspread client within these scoped credentials
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+#const to open spread sheet
+SHEET = GSPREAD_CLIENT.open('budget_planner')
+
+tracker = SHEET.worksheet('tracker')
+data = tracker.get_all_values() 
+
+print(data)
+
